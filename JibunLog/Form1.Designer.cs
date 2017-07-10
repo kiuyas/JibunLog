ae7c359dbd10384527afd527922b0f5756cd7370
kiuyas <kiuyas2@gmail.com>
2017-07-11 07:41:28 +0900

    書記コミット

---
+++ JibunLog/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace JibunLog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtViewer = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtViewer
+            // 
+            this.txtViewer.Font = new System.Drawing.Font("ＭＳ ゴシック", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtViewer.Location = new System.Drawing.Point(8, 8);
+            this.txtViewer.Multiline = true;
+            this.txtViewer.Name = "txtViewer";
+            this.txtViewer.ReadOnly = true;
+            this.txtViewer.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtViewer.Size = new System.Drawing.Size(608, 352);
+            this.txtViewer.TabIndex = 0;
+            this.txtViewer.TabStop = false;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("ＭＳ ゴシック", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtInput.Location = new System.Drawing.Point(8, 368);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInput.Size = new System.Drawing.Size(552, 64);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(568, 368);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(48, 64);
+            this.btnWrite.TabIndex = 2;
+            this.btnWrite.Text = "書込";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtViewer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "自分ログ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtViewer;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnWrite;
+    }
+}
+
---
+++ JibunLog/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace JibunLog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ShowLog();
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = txtInput.Text.Trim();
+            if (txtInput.Text != "" )
+            {
+                if (Write(txtInput.Text))
+                {
+                    txtInput.Clear();
+                    ShowLog();
+                }
+            }
+        }
+
+        private string GetFolder()
+        {
+            return Environment.CurrentDirectory;
+        }
+
+        private string GetLogFilePath()
+        {
+            return string.Format("{0}\\{1}\\{2}.txt", GetFolder(), DateTime.Today.ToString("yyyy"), DateTime.Today.ToString("yyyyMM"));
+        }
+
+        private String GetHeader()
+        {
+            return string.Format("[{0}] ", DateTime.Now.ToString("MM/dd HH:mm:ss"));
+        }
+
+        private bool Write(string text)
+        {
+            bool result = false;
+
+            try
+            {
+                string filePath = GetLogFilePath();
+                string folderPath = Path.GetDirectoryName(filePath);
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                using (StreamWriter sw = new StreamWriter(filePath, true))
+                {
+                    sw.WriteLine(GetHeader());
+                    sw.WriteLine(text);
+                    sw.WriteLine();
+                }
+
+                result = true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            return result;
+        }
+
+        private void ShowLog()
+        {
+            string filePath = GetLogFilePath();
+
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    using (StreamReader sr = new StreamReader(filePath))
+                    {
+                        txtViewer.Text = sr.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+    }
+}
