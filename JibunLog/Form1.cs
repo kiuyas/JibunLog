92847ea7b23903a858dcd32fb1b1ef4b3183649f
kiuyas <kiuyas2@gmail.com>
2020-02-01 16:31:45 +0900

    出力処理をリファクタリング

--- JibunLog/Form1.cs
+++ JibunLog/Form1.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
 using System.Windows.Forms;
 
 namespace JibunLog
@@ -10,9 +9,6 @@ namespace JibunLog
     /// </summary>
     public partial class Form1 : Form
     {
-        /// <summary>ログフォルダ名</summary>
-        private const string LOG_FOLDER_NAME = "logs";
-
         /// <summary>
         /// コンストラクタ
         /// </summary>
@@ -28,6 +24,14 @@ namespace JibunLog
         /// <param name="e">イベント引数</param>
         private void Form1_Load(object sender, EventArgs e)
         {
+            string logFolderPath = LogFileManager.GetLogFolder();
+
+            if (logFolderPath == null)
+            {
+                MessageBox.Show(this, "ログフォルダを作成できません。終了します。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Dispose();
+            }
+
             ShowLog();
         }
 
@@ -38,15 +42,17 @@ namespace JibunLog
         /// <param name="e">イベント引数</param>
         private void btnWrite_Click(object sender, EventArgs e)
         {
-            // 入力内容をトリムする。
-            txtInput.Text = txtInput.Text.Trim();
-
-            // 入力内容が空でない場合は以下の処理を実行する。
-            if (txtInput.Text != "" )
+            try
             {
-                // 入力内容を渡してWriteメソッドを呼び出し、戻り値がtrueであれば以下の処理を実行する。
-                if (Write(txtInput.Text))
+                // 入力内容をトリムする。
+                txtInput.Text = txtInput.Text.Trim();
+
+                // 入力内容が空でない場合は以下の処理を実行する。
+                if (txtInput.Text != "")
                 {
+                    // 入力内容を渡してWriteメソッドを呼び出し、戻り値がtrueであれば以下の処理を実行する。
+                    LogFileManager.Write(txtInput.Text);
+
                     // 入力欄をクリアする。
                     txtInput.Clear();
 
@@ -54,96 +60,10 @@ namespace JibunLog
                     ShowLog();
                 }
             }
-        }
-
-        /// <summary>
-        /// ログフォルダのパスを取得します。
-        /// フォルダが存在しない場合は作成します。
-        /// </summary>
-        /// <returns>カレントフォルダのパス</returns>
-        private string GetLogFolder()
-        {
-            string folderPath = Environment.CurrentDirectory + Path.DirectorySeparatorChar + LOG_FOLDER_NAME;
-
-            bool available = true;
-            if (File.Exists(folderPath))
-            {
-                available = false;
-            }
-            else if (!Directory.Exists(folderPath))
-            {
-                try
-                {
-                    Directory.CreateDirectory(folderPath);
-                }
-                catch(Exception)
-                {
-                    available = false;
-                }
-            }
-
-            if (!available)
-            {
-                MessageBox.Show(this, "ログフォルダを作成できません。終了します。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                folderPath = null;
-                Dispose();
-            }
-
-            return folderPath;
-        }
-
-
-        /// <summary>
-        /// ログ出力パスを取得します。
-        /// </summary>
-        /// <returns>ログ出力パス</returns>
-        private string GetLogFilePath()
-        {
-            return string.Format("{0}\\{1}\\{2}.txt", GetLogFolder(), DateTime.Today.ToString("yyyy"), DateTime.Today.ToString("yyyyMM"));
-        }
-
-        /// <summary>
-        /// ヘッダ文字列を取得します。
-        /// </summary>
-        /// <returns>ヘッダ文字列</returns>
-        private string GetHeader()
-        {
-            return string.Format("[{0}] ", DateTime.Now.ToString("MM/dd HH:mm:ss"));
-        }
-
-        /// <summary>
-        /// ログを書き込みます。
-        /// </summary>
-        /// <param name="text">ログ文字列</param>
-        /// <returns>処理結果</returns>
-        private bool Write(string text)
-        {
-            bool result = false;
-
-            try
-            {
-                string filePath = GetLogFilePath();
-                string folderPath = Path.GetDirectoryName(filePath);
-                if (!Directory.Exists(folderPath))
-                {
-                    Directory.CreateDirectory(folderPath);
-                }
-
-                using (StreamWriter sw = new StreamWriter(filePath, true))
-                {
-                    sw.WriteLine(GetHeader());
-                    sw.WriteLine(text);
-                    sw.WriteLine();
-                }
-
-                result = true;
-            }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString());
             }
-
-            return result;
         }
 
         /// <summary>
@@ -151,17 +71,9 @@ namespace JibunLog
         /// </summary>
         private void ShowLog()
         {
-            string filePath = GetLogFilePath();
-
             try
             {
-                if (File.Exists(filePath))
-                {
-                    using (StreamReader sr = new StreamReader(filePath))
-                    {
-                        txtViewer.Text = sr.ReadToEnd();
-                    }
-                }
+                txtViewer.Text = LogFileManager.Read();
             }
             catch (Exception ex)
             {
@@ -177,7 +89,7 @@ namespace JibunLog
         /// <param name="e">イベント引数</param>
         private void btnLogFolder_Click(object sender, EventArgs e)
         {
-            Process.Start(GetLogFolder());
+            Process.Start(LogFileManager.GetLogFolder());
         }
 
     }
---
+++ JibunLog/LogFileManager.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+
+namespace JibunLog
+{
+    /// <summary>
+    /// ログアクセスクラス
+    /// </summary>
+    class LogFileManager
+    {
+        /// <summary>ログフォルダ名</summary>
+        private const string LOG_FOLDER_NAME = "logs";
+
+        /// <summary>
+        /// ログフォルダのパスを取得します。
+        /// フォルダが存在しない場合は作成します。
+        /// </summary>
+        /// <returns>カレントフォルダのパス</returns>
+        public static string GetLogFolder()
+        {
+            return GetLogFolder(null);
+        }
+
+        /// <summary>
+        /// ログフォルダのパスを取得します。
+        /// フォルダが存在しない場合は作成します。
+        /// </summary>
+        /// <returns>カレントフォルダのパス</returns>
+        public static string GetLogFolder(string subFolderName)
+        {
+            string folderPath = Environment.CurrentDirectory + Path.DirectorySeparatorChar + LOG_FOLDER_NAME;
+            if (!string.IsNullOrEmpty(subFolderName))
+            {
+                folderPath += Path.DirectorySeparatorChar + subFolderName;
+            }
+
+            if (File.Exists(folderPath))
+            {
+                folderPath = null;
+            }
+            else if (!Directory.Exists(folderPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+                catch (Exception)
+                {
+                    folderPath = null;
+                }
+            }
+
+            return folderPath;
+        }
+
+        /// <summary>
+        /// ログ出力パスを取得します。
+        /// </summary>
+        /// <returns>ログ出力パス</returns>
+        public static string GetLogFilePath()
+        {
+            string folderPath = GetLogFolder(DateTime.Today.ToString("yyyy"));
+            if (folderPath == null)
+            {
+                return null;
+            }
+            else
+            {
+                return string.Format("{0}\\{1}.txt", folderPath, DateTime.Today.ToString("yyyyMM"));
+            }
+        }
+
+        /// <summary>
+        /// ログを書き込みます。
+        /// </summary>
+        /// <param name="text">ログ文字列</param>
+        /// <returns>処理結果</returns>
+        public static void Write(string text)
+        {
+            Write(GetLogFilePath(), text);
+        }
+
+        /// <summary>
+        /// ログを書き込みます。
+        /// </summary>
+        /// <param name="filePath">ログファイルパス</param>
+        /// <param name="text">ログ文字列</param>
+        /// <returns>処理結果</returns>
+        public static void Write(string filePath, string text)
+        {
+            using (StreamWriter sw = new StreamWriter(filePath, true))
+            {
+                sw.WriteLine(GetHeader());
+                sw.WriteLine(text);
+                sw.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// ヘッダ文字列を取得します。
+        /// </summary>
+        /// <returns>ヘッダ文字列</returns>
+        private static string GetHeader()
+        {
+            return string.Format("[{0}] ", DateTime.Now.ToString("MM/dd HH:mm:ss"));
+        }
+
+        /// <summary>
+        /// ログを読み込みます。
+        /// </summary>
+        /// <returns></returns>
+        public static string Read()
+        {
+            return Read(GetLogFilePath());
+        }
+
+        /// <summary>
+        /// ログを読み込みます。
+        /// </summary>
+        /// <param name="filePath">ログファイルパス</param>
+        /// <returns></returns>
+        public static string Read(string filePath)
+        {
+            string result = null;
+
+            if (File.Exists(filePath))
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    result = sr.ReadToEnd();
+                }
+            }
+
+            return result;
+        }
+    }
+}
